8d738994517034beef9131c90055a492c0c7e536
urbanijose <urbanijosemaria6@gmail.com>
2026-06-13 14:37:58 -0300

    Add files via upload

---
+++ Ing_Parcial2_Tema_E/src/ing_parcial2_tema_e/Ing_Parcial2_Tema_E.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ing_parcial2_tema_e;
+
+/**
+ *
+ * @author urban
+ */
+public class Ing_Parcial2_Tema_E {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
